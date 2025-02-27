8496843ee25abc3107f3f778381c13048ac8cc33
10317 <10317@proton.me>
2025-02-26 18:23:40 -0600

    deleted:    http/.mpd

--- http/response.go
+++ http/response.go
@@ -3,60 +3,33 @@ package http
 import (
    "bufio"
    "bytes"
-   "io"
-   "log"
    "net/http"
    "net/url"
-   "os"
 )
 
-func Read(r io.Reader) (*http.Response, error) {
-   buf := bufio.NewReader(r)
+func Marshal(resp *http.Response) ([]byte, error) {
+   data, err := resp.Request.URL.MarshalBinary()
+   if err != nil {
+      return nil, err
+   }
+   buf := bytes.NewBuffer(append(data, 0))
+   err = resp.Write(buf)
+   if err != nil {
+      return nil, err
+   }
+   return buf.Bytes(), nil
+}
+
+func Unmarshal(data []byte) (*http.Response, error) {
+   buf := bufio.NewReader(bytes.NewReader(data))
    data, err := buf.ReadSlice(0)
    if err != nil {
       return nil, err
    }
-   data = bytes.TrimSuffix(data, []byte{0})
    var u url.URL
-   err = u.UnmarshalBinary(data)
+   err = u.UnmarshalBinary(bytes.TrimSuffix(data, []byte{0}))
    if err != nil {
       return nil, err
    }
    return http.ReadResponse(buf, &http.Request{URL: &u})
-}
-
-// we cannot use os.Open because the full file is not read until the response
-// body is read
-func ReadFile(name string) (*http.Response, error) {
-   data, err := os.ReadFile(name)
-   if err != nil {
-      return nil, err
-   }
-   return Read(bytes.NewReader(data))
-}
-
-func Write(w io.Writer, resp *http.Response) error {
-   data, err := resp.Request.URL.MarshalBinary()
-   if err != nil {
-      return err
-   }
-   _, err = w.Write(data)
-   if err != nil {
-      return err
-   }
-   _, err = w.Write([]byte{0})
-   if err != nil {
-      return err
-   }
-   return resp.Write(w)
-}
-
-func WriteFile(name string, resp *http.Response) error {
-   log.Println("Create", name)
-   file, err := os.Create(name)
-   if err != nil {
-      return err
-   }
-   defer file.Close()
-   return Write(file, resp)
 }
--- http/response_test.go
+++ http/response_test.go
@@ -1,16 +1,29 @@
 package http
 
 import (
+   "net/http"
    "os"
+   "strings"
    "testing"
 )
 
 func TestResponse(t *testing.T) {
-   resp, err := ReadFile(".mpd")
+   resp, err := http.Post(
+      "http://httpbingo.org/post", "text/plain",
+      strings.NewReader("hello world"),
+   )
    if err != nil {
       t.Fatal(err)
    }
-   err = resp.Write(os.Stdout)
+   data, err := Marshal(resp)
+   if err != nil {
+      t.Fatal(err)
+   }
+   resp1, err := Unmarshal(data)
+   if err != nil {
+      t.Fatal(err)
+   }
+   err = resp1.Write(os.Stdout)
    if err != nil {
       t.Fatal(err)
    }
