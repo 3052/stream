95b6252980d3ddacaf7d0d0b147d7282ec414eef
367 <367@tuta.io>
2025-04-26 15:33:09 -0500

    +func (s *Segment) Next() {

--- progress/progress.go
+++ progress/progress.go
@@ -7,36 +7,6 @@ import (
    "time"
 )
 
-// firefox does this
-// 29s left - 19.9 of 33.5 MB (540 KB/sec)
-
-func (s *Segment) Next() {
-   s.segmentA++
-   s.segmentB--
-   timeB := time.Now().Unix()
-   if timeB > s.timeB {
-      log.Println(
-         s.segmentB, "segment",
-         s.durationB().Truncate(time.Second),
-         "left",
-      )
-      s.timeB = timeB
-   }
-}
-
-func (s *Segment) Set(segmentB int) {
-   s.segmentB = segmentB
-   s.timeA = time.Now()
-   s.timeB = time.Now().Unix()
-}
-
-type Segment struct {
-   segmentA int64
-   segmentB int
-   timeA    time.Time
-   timeB    int64
-}
-
 func (b *Byte) Read(data []byte) (int, error) {
    n, err := b.read.Read(data)
    b.byteA += int64(n)
@@ -71,6 +41,38 @@ func (b *Byte) Set(resp *http.Response) {
 func (b *Byte) durationA() time.Duration {
    return time.Since(b.timeA)
 }
+
+// firefox does this
+// 29s left - 19.9 of 33.5 MB (540 KB/sec)
+
+func (s *Segment) Next() {
+   s.segmentA++
+   s.segmentB--
+   timeB := time.Now().Unix()
+   if timeB > s.timeB {
+      log.Println(
+         s.segmentB, "segment",
+         s.durationB().Truncate(time.Second),
+         "left",
+      )
+      s.timeB = timeB
+   }
+}
+
+func (s *Segment) Set(segmentB int) {
+   s.segmentB = segmentB
+   s.timeA = time.Now()
+   s.timeB = time.Now().Unix()
+}
+
+type Segment struct {
+   segmentA int64
+   segmentB int
+   timeA    time.Time
+   timeB    int64
+}
+
+///
 
 func (s *Segment) durationA() time.Duration {
    return time.Since(s.timeA)
