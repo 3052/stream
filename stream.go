37cc84d90658f0b71b0bb25a97351ba7d3c3edee
367 <367@tuta.io>
2025-05-16 14:29:32 -0500

    new file:   license.go

--- stream.go
+++ stream.go
@@ -4,7 +4,6 @@ import (
    "41.neocities.org/dash"
    "41.neocities.org/sofia/file"
    "41.neocities.org/sofia/pssh"
-   "41.neocities.org/widevine"
    "bufio"
    "bytes"
    "encoding/base64"
@@ -19,105 +18,6 @@ import (
    "strings"
    "time"
 )
-
-// firefox does this
-// 29s left - 19.9 of 33.5 MB (540 KB/sec)
-
-func (p *progress) Next() {
-   p.segmentA++
-   p.segmentB--
-   timeB := time.Now().Unix()
-   if timeB > p.timeB {
-      log.Println(
-         p.segmentB, "segment",
-         p.durationB().Truncate(time.Second),
-         "left",
-      )
-      p.timeB = timeB
-   }
-}
-
-func (p *progress) Set(segmentB int) {
-   p.segmentB = segmentB
-   p.timeA = time.Now()
-   p.timeB = time.Now().Unix()
-}
-
-type progress struct {
-   segmentA int64
-   segmentB int
-   timeA    time.Time
-   timeB    int64
-}
-
-func (p *progress) durationA() time.Duration {
-   return time.Since(p.timeA)
-}
-
-// keep last two terms separate
-func (p *progress) durationB() time.Duration {
-   return p.durationA() * time.Duration(p.segmentB) / time.Duration(p.segmentA)
-}
-type representation struct {
-   bandwidth int
-   codecs    string
-}
-
-func (r *representation) variation(expect int) int {
-   variation := r.bandwidth - expect
-   if variation < 0 {
-      return -variation
-   }
-   return variation
-}
-
-func (a *representation) expect(values []representation, expect int) {
-   *a = values[0]
-   for _, b := range values[1:] {
-      if b.variation(expect) < a.variation(expect) {
-         *a = b
-      }
-   }
-}
-
-// github.com/golang/go/blob/go1.24.3/src/math/all_test.go#L2146
-func (r *representation) tolerance(expect int, percent float64) bool {
-   return float64(r.variation(expect)) <= float64(expect)*percent
-}
-// segment can be VTT or anything
-func (m *media_file) write_segment(data, key []byte) ([]byte, error) {
-   if key == nil {
-      return data, nil
-   }
-   var file1 file.File
-   err := file1.Read(data)
-   if err != nil {
-      return nil, err
-   }
-   if m.duration/m.timescale < 10*60 {
-      for _, sample := range file1.Moof.Traf.Trun.Sample {
-         if sample.Duration == 0 {
-            sample.Duration = file1.Moof.Traf.Tfhd.DefaultSampleDuration
-         }
-         m.duration += uint64(sample.Duration)
-         if sample.Size == 0 {
-            sample.Size = file1.Moof.Traf.Tfhd.DefaultSampleSize
-         }
-         m.size += uint64(sample.Size)
-      }
-      log.Println("bandwidth", m.timescale*m.size*8/m.duration)
-   }
-   if file1.Moof.Traf.Senc == nil {
-      return data, nil
-   }
-   for i, data := range file1.Mdat.Data(&file1.Moof.Traf) {
-      err = file1.Moof.Traf.Senc.Sample[i].Decrypt(data, key)
-      if err != nil {
-         return nil, err
-      }
-   }
-   return file1.Append(nil)
-}
 
 var ThreadCount = 1
 
@@ -155,233 +55,6 @@ func Mpd(name string, resp *http.Response) error {
          fmt.Println()
       }
       fmt.Println(&represent)
-   }
-   return nil
-}
-
-func (e *License) segment_template(represent *dash.Representation) error {
-   var media media_file
-   err := media.New(represent)
-   if err != nil {
-      return err
-   }
-   file1, err := dash_create(represent)
-   if err != nil {
-      return err
-   }
-   defer file1.Close()
-   if initial := represent.SegmentTemplate.Initialization; initial != "" {
-      address, err := initial.Url(represent)
-      if err != nil {
-         return err
-      }
-      data1, err := get(address, nil)
-      if err != nil {
-         return err
-      }
-      data1, err = media.initialization(data1)
-      if err != nil {
-         return err
-      }
-      _, err = file1.Write(data1)
-      if err != nil {
-         return err
-      }
-   }
-   key, err := e.get_key(&media)
-   if err != nil {
-      return err
-   }
-   head := http.Header{}
-   head.Set("silent", "true")
-   var segments []int
-   for r := range represent.Representation() {
-      segments = slices.AppendSeq(segments, r.Segment())
-   }
-   var progress1 progress
-   progress1.Set(len(segments))
-   for chunk := range slices.Chunk(segments, ThreadCount) {
-      var (
-         datas = make([][]byte, len(chunk))
-         errs = make(chan error)
-      )
-      for i, segment := range chunk {
-         address, err := represent.SegmentTemplate.Media.Url(represent, segment)
-         if err != nil {
-            return err
-         }
-         go func() {
-            datas[i], err = get(address, head)
-            errs <- err
-            progress1.Next()
-         }()
-      }
-      for range chunk {
-         err := <-errs
-         if err != nil {
-            return err
-         }
-      }
-      for _, data := range datas {
-         data, err = media.write_segment(data, key)
-         if err != nil {
-            return err
-         }
-         _, err = file1.Write(data)
-         if err != nil {
-            return err
-         }
-      }
-   }
-   return nil
-}
-
-func (e *License) segment_base(represent *dash.Representation) error {
-   if ThreadCount != 1 {
-      return errors.New("ThreadCount")
-   }
-   var media media_file
-   err := media.New(represent)
-   if err != nil {
-      return err
-   }
-   file1, err := dash_create(represent)
-   if err != nil {
-      return err
-   }
-   defer file1.Close()
-   data, err := get(represent.BaseUrl[0], http.Header{
-      "range": {"bytes=" + represent.SegmentBase.Initialization.Range},
-   })
-   if err != nil {
-      return err
-   }
-   data, err = media.initialization(data)
-   if err != nil {
-      return err
-   }
-   _, err = file1.Write(data)
-   if err != nil {
-      return err
-   }
-   key, err := e.get_key(&media)
-   if err != nil {
-      return err
-   }
-   data, err = get(represent.BaseUrl[0], http.Header{
-      "range": {"bytes=" + represent.SegmentBase.IndexRange},
-   })
-   if err != nil {
-      return err
-   }
-   var file2 file.File
-   err = file2.Read(data)
-   if err != nil {
-      return err
-   }
-   var progress1 progress
-   progress1.Set(len(file2.Sidx.Reference))
-   head := http.Header{}
-   head.Set("silent", "true")
-   var index dash.Range
-   err = index.Set(represent.SegmentBase.IndexRange)
-   if err != nil {
-      return err
-   }
-   for _, reference := range file2.Sidx.Reference {
-      index[0] = index[1] + 1
-      index[1] += uint64(reference.Size())
-      head.Set("range", "bytes="+ index.String())
-      data, err = get(represent.BaseUrl[0], head)
-      if err != nil {
-         return err
-      }
-      progress1.Next()
-      data, err = media.write_segment(data, key)
-      if err != nil {
-         return err
-      }
-      _, err = file1.Write(data)
-      if err != nil {
-         return err
-      }
-   }
-   return nil
-}
-
-func (e *License) segment_list(represent *dash.Representation) error {
-   if ThreadCount != 1 {
-      return errors.New("ThreadCount")
-   }
-   var media media_file
-   err := media.New(represent)
-   if err != nil {
-      return err
-   }
-   file1, err := dash_create(represent)
-   if err != nil {
-      return err
-   }
-   defer file1.Close()
-   data, err := get(represent.SegmentList.Initialization.SourceUrl[0], nil)
-   if err != nil {
-      return err
-   }
-   data, err = media.initialization(data)
-   if err != nil {
-      return err
-   }
-   _, err = file1.Write(data)
-   if err != nil {
-      return err
-   }
-   key, err := e.get_key(&media)
-   if err != nil {
-      return err
-   }
-   var progress1 progress
-   progress1.Set(len(represent.SegmentList.SegmentUrl))
-   head := http.Header{}
-   head.Set("silent", "true")
-   for _, segment := range represent.SegmentList.SegmentUrl {
-      data, err := get(segment.Media[0], head)
-      if err != nil {
-         return err
-      }
-      progress1.Next()
-      data, err = media.write_segment(data, key)
-      if err != nil {
-         return err
-      }
-      _, err = file1.Write(data)
-      if err != nil {
-         return err
-      }
-   }
-   return nil
-}
-
-func (m *media_file) New(represent *dash.Representation) error {
-   for _, content := range represent.ContentProtection {
-      if content.SchemeIdUri == widevine_urn {
-         if content.Pssh != "" {
-            data, err := base64.StdEncoding.DecodeString(content.Pssh)
-            if err != nil {
-               return err
-            }
-            var box pssh.Box
-            n, err := box.BoxHeader.Decode(data)
-            if err != nil {
-               return err
-            }
-            err = box.Read(data[n:])
-            if err != nil {
-               return err
-            }
-            m.pssh = box.Data
-            break
-         }
-      }
    }
    return nil
 }
@@ -443,6 +116,55 @@ func unmarshal(data []byte) (*http.Response, error) {
    )
 }
 
+func dash_create(represent *dash.Representation) (*os.File, error) {
+   switch *represent.MimeType {
+   case "audio/mp4":
+      return create(".m4a")
+   case "text/vtt":
+      return create(".vtt")
+   case "video/mp4":
+      return create(".m4v")
+   }
+   return nil, errors.New(*represent.MimeType)
+}
+
+func init() {
+   log.SetFlags(log.Ltime)
+   http.DefaultClient.Transport = &transport{
+      // x/net/http2: make Transport return nicer error when Amazon ALB hangs up
+      // mid-response?
+      // github.com/golang/go/issues/18639
+      // x/net/http2: Transport ignores net/http.Transport.Proxy once connected
+      // github.com/golang/go/issues/25793
+      Protocols: &http.Protocols{},
+      Proxy:     http.ProxyFromEnvironment,
+   }
+}
+
+func (m *media_file) New(represent *dash.Representation) error {
+   for _, content := range represent.ContentProtection {
+      if content.SchemeIdUri == widevine_urn {
+         if content.Pssh != "" {
+            data, err := base64.StdEncoding.DecodeString(content.Pssh)
+            if err != nil {
+               return err
+            }
+            var box pssh.Box
+            n, err := box.BoxHeader.Decode(data)
+            if err != nil {
+               return err
+            }
+            err = box.Read(data[n:])
+            if err != nil {
+               return err
+            }
+            m.pssh = box.Data
+            break
+         }
+      }
+   }
+   return nil
+}
 type media_file struct {
    key_id    []byte // tenc
    pssh      []byte // pssh
@@ -489,69 +211,75 @@ func (m *media_file) initialization(data []byte) ([]byte, error) {
    copy(sample.BoxHeader.Type[:], sinf.Frma.DataFormat[:]) // Firefox
    return file1.Append(nil)
 }
-
-type License struct {
-   ClientId   string
-   PrivateKey string
-   Widevine   func([]byte) ([]byte, error)
-}
-func (e *License) Download(name, id string) error {
-   data, err := os.ReadFile(name)
-   if err != nil {
-      return err
+// segment can be VTT or anything
+func (m *media_file) write_segment(data, key []byte) ([]byte, error) {
+   if key == nil {
+      return data, nil
    }
-   resp, err := unmarshal(data)
+   var file1 file.File
+   err := file1.Read(data)
    if err != nil {
-      return err
+      return nil, err
    }
-   defer resp.Body.Close()
-   data, err = io.ReadAll(resp.Body)
-   if err != nil {
-      return err
-   }
-   var mpd1 dash.Mpd
-   err = mpd1.Unmarshal(data)
-   if err != nil {
-      return err
-   }
-   mpd1.Set(resp.Request.URL)
-   for represent := range mpd1.Representation() {
-      if represent.Id == id {
-         if represent.SegmentBase != nil {
-            return e.segment_base(&represent)
+   if m.duration/m.timescale < 10*60 {
+      for _, sample := range file1.Moof.Traf.Trun.Sample {
+         if sample.Duration == 0 {
+            sample.Duration = file1.Moof.Traf.Tfhd.DefaultSampleDuration
          }
-         if represent.SegmentList != nil {
-            return e.segment_list(&represent)
+         m.duration += uint64(sample.Duration)
+         if sample.Size == 0 {
+            sample.Size = file1.Moof.Traf.Tfhd.DefaultSampleSize
          }
-         return e.segment_template(&represent)
+         m.size += uint64(sample.Size)
+      }
+      log.Println("bandwidth", m.timescale*m.size*8/m.duration)
+   }
+   if file1.Moof.Traf.Senc == nil {
+      return data, nil
+   }
+   for i, data := range file1.Mdat.Data(&file1.Moof.Traf) {
+      err = file1.Moof.Traf.Senc.Sample[i].Decrypt(data, key)
+      if err != nil {
+         return nil, err
       }
    }
-   return nil
+   return file1.Append(nil)
 }
 
-func dash_create(represent *dash.Representation) (*os.File, error) {
-   switch *represent.MimeType {
-   case "audio/mp4":
-      return create(".m4a")
-   case "text/vtt":
-      return create(".vtt")
-   case "video/mp4":
-      return create(".m4v")
+func (p *progress) next() {
+   p.segmentA++
+   p.segmentB--
+   timeB := time.Now().Unix()
+   if timeB > p.timeB {
+      log.Println(
+         p.segmentB, "segment",
+         p.durationB().Truncate(time.Second),
+         "left",
+      )
+      p.timeB = timeB
    }
-   return nil, errors.New(*represent.MimeType)
 }
 
-func init() {
-   log.SetFlags(log.Ltime)
-   http.DefaultClient.Transport = &transport{
-      // x/net/http2: make Transport return nicer error when Amazon ALB hangs up
-      // mid-response?
-      // github.com/golang/go/issues/18639
-      // x/net/http2: Transport ignores net/http.Transport.Proxy once connected
-      // github.com/golang/go/issues/25793
-      Protocols: &http.Protocols{},
-      Proxy:     http.ProxyFromEnvironment,
-   }
+func (p *progress) set(segmentB int) {
+   p.segmentB = segmentB
+   p.timeA = time.Now()
+   p.timeB = time.Now().Unix()
+}
+
+type progress struct {
+   segmentA int64
+   segmentB int
+   timeA    time.Time
+   timeB    int64
+}
+
+func (p *progress) durationA() time.Duration {
+   return time.Since(p.timeA)
+}
+
+// keep last two terms separate
+func (p *progress) durationB() time.Duration {
+   return p.durationA() * time.Duration(p.segmentB) / time.Duration(p.segmentA)
 }
 
 type transport http.Transport
@@ -563,55 +291,25 @@ func (t *transport) RoundTrip(req *http.Request) (*http.Response, error) {
    return (*http.Transport)(t).RoundTrip(req)
 }
 
-func (e *License) get_key(media *media_file) ([]byte, error) {
-   if media.key_id == nil {
-      return nil, nil
+func variation(value *dash.Representation, expect int) int {
+   variation := value.Bandwidth - expect
+   if variation < 0 {
+      return -variation
    }
-   private_key, err := os.ReadFile(e.PrivateKey)
-   if err != nil {
-      return nil, err
-   }
-   client_id, err := os.ReadFile(e.ClientId)
-   if err != nil {
-      return nil, err
-   }
-   if media.pssh == nil {
-      var pssh1 widevine.Pssh
-      pssh1.KeyIds = [][]byte{media.key_id}
-      media.pssh = pssh1.Marshal()
-   }
-   log.Println("PSSH", base64.StdEncoding.EncodeToString(media.pssh))
-   var module widevine.Cdm
-   err = module.New(private_key, client_id, media.pssh)
-   if err != nil {
-      return nil, err
-   }
-   data, err := module.RequestBody()
-   if err != nil {
-      return nil, err
-   }
-   data, err = e.Widevine(data)
-   if err != nil {
-      return nil, err
-   }
-   var body widevine.ResponseBody
-   err = body.Unmarshal(data)
-   if err != nil {
-      return nil, err
-   }
-   block, err := module.Block(body)
-   if err != nil {
-      return nil, err
-   }
-   for container := range body.Container() {
-      if bytes.Equal(container.Id(), media.key_id) {
-         key := container.Key(block)
-         log.Println("key", base64.StdEncoding.EncodeToString(key))
-         var zero [16]byte
-         if !bytes.Equal(key, zero[:]) {
-            return key, nil
-         }
+   return variation
+}
+
+func expected(values []dash.Representation, expect int) dash.Representation {
+   a := values[0]
+   for _, b := range values[1:] {
+      if variation(&b, expect) < variation(&a, expect) {
+         a = b
       }
    }
-   return nil, errors.New("get_key")
+   return a
+}
+
+// github.com/golang/go/blob/go1.24.3/src/math/all_test.go#L2146
+func tolerance(value *dash.Representation, expect int, percent float64) bool {
+   return float64(variation(value, expect)) <= float64(expect)*percent
 }
