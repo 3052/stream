cad2b9bae26d3be15d9dc63db3739dc95bbcf8ee
367 <367@tuta.io>
2025-05-16 13:52:19 -0500

    deleted:    internal/dash.go

--- stream.go
+++ stream.go
@@ -17,8 +17,73 @@ import (
    "os"
    "slices"
    "strings"
+   "time"
 )
 
+// firefox does this
+// 29s left - 19.9 of 33.5 MB (540 KB/sec)
+
+func (p *progress) Next() {
+   p.segmentA++
+   p.segmentB--
+   timeB := time.Now().Unix()
+   if timeB > p.timeB {
+      log.Println(
+         p.segmentB, "segment",
+         p.durationB().Truncate(time.Second),
+         "left",
+      )
+      p.timeB = timeB
+   }
+}
+
+func (p *progress) Set(segmentB int) {
+   p.segmentB = segmentB
+   p.timeA = time.Now()
+   p.timeB = time.Now().Unix()
+}
+
+type progress struct {
+   segmentA int64
+   segmentB int
+   timeA    time.Time
+   timeB    int64
+}
+
+func (p *progress) durationA() time.Duration {
+   return time.Since(p.timeA)
+}
+
+// keep last two terms separate
+func (p *progress) durationB() time.Duration {
+   return p.durationA() * time.Duration(p.segmentB) / time.Duration(p.segmentA)
+}
+type representation struct {
+   bandwidth int
+   codecs    string
+}
+
+func (r *representation) variation(expect int) int {
+   variation := r.bandwidth - expect
+   if variation < 0 {
+      return -variation
+   }
+   return variation
+}
+
+func (a *representation) expect(values []representation, expect int) {
+   *a = values[0]
+   for _, b := range values[1:] {
+      if b.variation(expect) < a.variation(expect) {
+         *a = b
+      }
+   }
+}
+
+// github.com/golang/go/blob/go1.24.3/src/math/all_test.go#L2146
+func (r *representation) tolerance(expect int, percent float64) bool {
+   return float64(r.variation(expect)) <= float64(expect)*percent
+}
 // segment can be VTT or anything
 func (m *media_file) write_segment(data, key []byte) ([]byte, error) {
    if key == nil {
--- stream_test.go
+++ stream_test.go
@@ -2,10 +2,36 @@ package dash
 
 import (
    "fmt"
+   "io"
+   "log"
+   "net/http"
    "testing"
 )
 
-func Test(t *testing.T) {
+func TestProgress(t *testing.T) {
+   log.SetFlags(log.Ltime)
+   var (
+      segment    [9]struct{}
+      progress1 progress
+   )
+   progress1.Set(len(segment))
+   for range segment {
+      func() {
+         resp, err := http.Get("http://httpbingo.org/drip?delay=0&duration=1")
+         if err != nil {
+            t.Fatal(err)
+         }
+         defer resp.Body.Close()
+         _, err = io.Copy(io.Discard, resp.Body)
+         if err != nil {
+            t.Fatal(err)
+         }
+      }()
+      progress1.Next()
+   }
+}
+
+func TestExpect(t *testing.T) {
    const expect = 3_300_000
    var actual representation
    actual.expect(representations, expect)
